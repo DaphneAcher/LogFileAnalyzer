5c166698aca54bb83d46bc8f6dba15b87e91701f
Daphne Acher <191528512+DaphneAcher@users.noreply.github.com>
2025-02-13 19:55:21 -0500

    commit

---
+++ LogFileAnalyzer.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+input_file = "error.txt"
+
+error_dict = defaultdict(int)
+
+try:
+    input_file = open(input_file, 'r')
+    for line in input_file:
+                if "ERROR " in line:
+                    error_message = line.split("ERROR ", 1)[1].strip()
+                    error_dict[error_message] += 1
+                    
+    def get_count(item):
+        return item[1]
+
+    sorted_errors = sorted(error_dict.items(), key=get_count, reverse=True)
+
+    print("\nTop 3 most frequent error messages:")
+    for i in range(min(3, len(sorted_errors))):
+        message, count = sorted_errors[i]
+        print(f"{i+1}. {message} - {count} occurrences")
+
+except FileNotFoundError:
+    print("This files doesn't exist")
+
